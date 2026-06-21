4821a98c1b7f20cfe318623b38a5414fae34bf7a
srikant-90 <srikantbehera1803@gmail.com>
2026-06-21 20:42:27 +0530

    [Syncode] GeeksForGeeks/Easy/Three way partitioning

---
+++ GeeksForGeeks/Easy/Three way partitioning/solution.java
@@ -0,0 +1,35 @@
+import java.io.*;
+
+class Solution {
+    // Function to partition the array around the range such
+    // that array is divided into three parts.
+    public void threeWayPartition(int arr[], int a, int b) {
+        // code here
+        int n=arr.length;
+        int start=0,end=n-1;
+        
+        for(int i=0;i<=end;)
+        {
+            if(arr[i]<a)
+            {
+                int temp=arr[start];
+                arr[start]=arr[i];
+                arr[i]=temp;
+                start++;
+                i++;
+                
+            }
+            else if(arr[i]>b)
+            {
+                int temp=arr[end];
+                arr[end]=arr[i];
+                arr[i]=temp;
+                end--;
+            }
+            else
+            {
+                i++;
+            }
+        }
+    }
+}
