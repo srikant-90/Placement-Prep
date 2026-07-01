8b5209d5c36482e8c75f2b09b356abbb907f4bbb
srikant-90 <srikantbehera1803@gmail.com>
2026-07-01 20:38:20 +0530

    [Syncode] LeetCode/Medium/Find First and Last Position of Element in Sorted Array

---
+++ LeetCode/Medium/Find First and Last Position of Element in Sorted Array/solution.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int[] res= new int[]{-1,-1};
+        if(nums.length==0) return res;
+        int start=0,end=nums.length-1;
+        while(start<end)
+        {
+            int mid=start+(end-start)/2;
+            if(nums[mid]>=target)
+            {
+                end=mid;
+            }
+            else
+            {
+                start=mid+1;
+            }
+        }
+        if(nums[start]!=target)
+        {
+            return res;
+        }
+        res[0]=start;
+        end=nums.length-1;
+        while(start<end)
+        {
+            int mid=start+(end-start+1)/2;
+            if(nums[mid]>target)
+            {
+                end=mid-1;
+            }
+            else
+            {
+                start=mid;
+            }
+        }
+        res[1]=start;
+        return res;
+    }
+}
