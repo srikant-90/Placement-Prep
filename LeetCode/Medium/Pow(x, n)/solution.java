14fcc9aa3da468005bc44684381ac834485abfae
srikant-90 <srikantbehera1803@gmail.com>
2026-07-27 14:05:10 +0530

    [Syncode] LeetCode/Medium/Pow(x, n)

---
+++ LeetCode/Medium/Pow(x, n)/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public double myPow(double x, int n) {
+       long temp=n;
+       if(temp<0)
+       {
+        return 1.0/power(x,-temp);
+
+       }
+       return power(x,temp);
+
+    }
+    public static double power(double x,long n)
+    {
+        if(n==0) return 1.0;
+        if(n==1) return x;
+        if(n%2==0)
+        {
+            return power(x*x,n/2);
+        }
+        return x*power(x,n-1);
+    }
+}
